6cd5a4480bf331d410705c1d4bd254d133fa7e14
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-07-15 10:57:40 -0500

    Experimenting with basic state manipulation

--- src/PrimaryBox.jsx
+++ src/PrimaryBox.jsx
@@ -2,9 +2,21 @@ import React from 'react';
 
 class PrimaryBox extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {isToggleOn: true};
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState(prev => ({
+            isToggleOn: !prev.isToggleOn
+        }));
+    }
+
     render() {
         return (
-            <div style={{backgroundColor: "red", width: "50px", height: "50px", fontSize: "20px", textAlign: 'center', paddingTop: "3em", padding: "3em", marginTop: "10px"}}>
+            <div onClick = {this.handleClick} style={{backgroundColor: this.state.isToggleOn ? "red" : "blue" , width: "50px", height: "50px", fontSize: "20px", textAlign: 'center', paddingTop: "3em", padding: "3em", marginTop: "10px"}}>
               It's a box!  
             </div>
 
