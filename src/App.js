6981b4ec7f1936c9e9971c519e771d719a45556e
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-07-16 10:52:43 -0500

    if-statement basic implementation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from './logo.svg';
 import './App.css';
 import PrimaryBox from './PrimaryBox.jsx'
 
@@ -17,6 +16,22 @@ const friends = [
     src: "https://content.codecademy.com/courses/React/react_photo-alpaca.jpg"
   }
 ];
+
+const fiftyFifty = Math.random() < 0.5;
+
+class TonightsPlan extends React.Component {
+  
+  render() {
+    let plan;
+    if (fiftyFifty) {
+      plan = "out";
+    } else {
+      plan = "to bed"
+    }
+  
+    return <h1>Tonight I'm going {plan} WOOO</h1>;
+  }  
+}
 
 class Friend extends React.Component {
 
@@ -36,6 +51,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <PrimaryBox></PrimaryBox>
+        <TonightsPlan />
         <Friend />
       </header>
       
