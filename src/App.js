2fc777698d2fdf99ceeee25e22cf76a8f7ac8a10
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-07-16 13:35:16 -0500

    Simple authorization form

--- src/App.js
+++ src/App.js
@@ -56,6 +56,42 @@ class Button extends React.Component {
   }
 }
 
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      password: 'swordfish',
+      authorized: false
+    };
+    this.authorize = this.authorize.bind(this);
+  }
+
+  authorize(e) {
+    const password = e.target.querySelector(
+      'input[type="password"]').value;
+    const auth = password == this.state.password;
+    this.setState({
+      authorized: auth
+    });
+  }
+
+  render() {
+    return (
+      <div id="authorization">
+        <h1>Contact</h1>
+        <ul>
+          <li>
+            client@example.com
+          </li>
+          <li>
+            555.555.5555
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
 function App() {
   return (
     <div className="App">
@@ -63,6 +99,7 @@ function App() {
         <PrimaryBox></PrimaryBox>
         <TonightsPlan />
         <Button />
+        <Contact />
         <Friend />
       </header>
       
